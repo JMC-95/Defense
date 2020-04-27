3f73581ea8c135d9d5a4f92f822e8e3b2a2810ed
zxwnstn <zxwnstn@naver.com>
2020-04-27 13:51:55 +0900

    merge complete

---
+++ Assets/Scripts/Type.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Type
+{
+    class Tower
+    {
+        public static int Arrow = 0;
+        public static int Cannon = 3;
+        public static int Max = Cannon + 1;
+    }
+
+}
